eb9c8fce7fe51b02b2d970f73ba973fa7807d90b
Jongbok Lee <s2feeling@naver.com>
2019-11-28 22:02:04 +0900

    init

---
+++ homework/index.js
@@ -0,0 +1,30 @@
+
+<script>
+	function hey(){
+		alert("안녕!");
+	}ㄴ
+
+	let num = 0;
+
+	function count_fruit(name){
+		let count = 0
+		for(let i = 0; i< a.length; i++){
+			if(a[i] == name){
+				count = count + 1;
+			}
+		}
+
+		console.log(count);
+	}
+
+
+
+	count_fruit('감');
+	count_fruit('수박');
+
+
+</script>
+
+//버튼 부분
+<button onclick = "hey()" class="btn btn-primary">기사저장</button>
+
